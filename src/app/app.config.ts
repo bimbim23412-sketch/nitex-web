648e1e44924cd2bdd15fa388b048e54403c7a604
angel <tu@email.com>
2026-05-06 10:24:08 -0400

    fix completo deploy sin animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, TitleStrategy } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { TemplatePageTitleStrategy } from './core/services/title.strategy';
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimationsAsync(),
     { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }
   ],
 };
